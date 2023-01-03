399106b412ee4e6faf597bc8a202b04034d15d3b
DESKTOP-2HGUL02\reinsp5 <sk.reinsp5@gmail.com>
2023-01-03 13:28:16 +0900

    description追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,14 @@ export default defineNuxtConfig({
   app: {
     head: {
       title: "Luminessチームサイト",
-      meta: [{ charset: "utf-8" }],
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "description",
+          content:
+            "PSO2およびPSO2NGSのShip5で活動中のチーム「Luminess」のWebサイト",
+        },
+      ],
       htmlAttrs: {
         lang: "ja",
         "data-theme": "corporate",
