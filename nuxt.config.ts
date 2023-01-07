b8c9599f7b2ae24f14f4c124f3d76e902f3320af
レイン <sk.reinsp5@gmail.com>
2023-01-07 13:57:04 +0900

    ファビコン設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       cmsBase: process.env.CMS_BASE_URL,
-      apiBase: `${process.env.CMS_BASE_URL}/api`
+      apiBase: `${process.env.CMS_BASE_URL}/api`,
     },
   },
   app: {
@@ -37,7 +37,36 @@ export default defineNuxtConfig({
         {
           name: "twitter:image",
           content:
-            "https://cms.luminess.games/uploads/twitter_preview_beff7dcb7f.webp",
+            "/twitter_preview.webp",
+        },
+      ],
+      link: [
+        {
+          rel: "apple-touch-icon",
+          type: "image/webp",
+          sizes: "180x180",
+          href: "/favicons/apple-touch-icon.webp",
+        },
+        {
+          rel: "icon",
+          type: "image/webp",
+          sizes: "32x32",
+          href: "/favicons/favicon-32x32.webp",
+        },
+        {
+          rel: "icon",
+          type: "image/webp",
+          sizes: "16x16",
+          href: "/favicons/favicon-16x16.webp",
+        },
+        {
+          rel: "icon",
+          type: "image/vnd.microsoft.icon",
+          href: "/favicons/favicon.ico",
+        },
+        {
+          rel: "manifest",
+          href: "/favicons/site.webmanifest",
         },
       ],
       htmlAttrs: {
