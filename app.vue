7f99350115b71ec031c69176f2acad78bd97c2f8
DESKTOP-2HGUL02\reinsp5 <sk.reinsp5@gmail.com>
2023-01-11 22:40:19 +0900

    ページ遷移アニメーション追加

--- app.vue
+++ app.vue
@@ -16,4 +16,14 @@ body,
 body {
   font-family: "BIZ UDPGothic", sans-serif;
 }
+
+.page-enter-active,
+.page-leave-active {
+  transition: all 150ms;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
 </style>
